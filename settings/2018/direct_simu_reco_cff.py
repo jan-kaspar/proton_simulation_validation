4fb7d562661283594322b692d6d6b9a83dbbbd25
Jan Kaspar <jan.kaspar@cern.ch>
2020-06-12 11:47:06 +0200

    Removed old 2018 profile.

--- settings/2018/direct_simu_reco_cff.py
+++
@@ -1,7 +0,0 @@
-from Configuration.Eras.Era_Run2_2018_cff import *
-era = Run2_2018
-
-def LoadConfig(process):
-  global config
-  import Validation.CTPPS.simu_config.year_2018_cff as config
-  process.load("Validation.CTPPS.simu_config.year_2018_cff")
